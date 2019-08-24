8dc7a4a39e2023a1eed71af574090fe6b81c90e9
annenini <ninifrancis15@gmail.com>
2019-08-24 14:51:54 +0300

    form to validate driving age

---
+++ script.js
@@ -0,0 +1,10 @@
+let age=prompt('Enter your Age');
+if(age==18){
+alert('Drive a small car');
+}
+else if(age>=21 && age<=28){
+alert('Drive a big car');
+}
+else if(age>=24){
+    document.write('Can drive a truck');
+}
